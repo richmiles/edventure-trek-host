f48d050df4799b1d4d2aa43791d5a3f25ce3c131
Rich Miles <me@richmiles.xyz>
2023-11-16 06:28:03 -0700

    Removed message logging from opening.

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Edventure Studios LLC-Edventure Trek-0.1";
+const cacheName = "Edventure Studios LLC-Edventure Trek-0.1.0-alpha+20231116035102";
 const contentToCache = [
     "Build/Web.loader.js",
     "Build/Web.framework.js",
