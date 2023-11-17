579363217deebea6bd17f7c6c33235142735a490
Rich Miles <me@richmiles.xyz>
2023-11-17 14:00:03 -0700

    Web now downloads mushroom mayhem remotely.

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Edventure Studios LLC-Edventure Trek-0.1.0-alpha+20231116035102";
+const cacheName = "Edventure Studios LLC-Edventure Trek-0.1.0-alpha+20231117201136";
 const contentToCache = [
     "Build/Web.loader.js",
     "Build/Web.framework.js",
