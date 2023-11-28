e9e340a51d6cabca7633922cb294bc515a977e61
Rich Miles <me@richmiles.xyz>
2023-11-28 16:09:50 -0700

    Removed Engine Stripping

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Edventure Studios LLC-Edventure Trek-0.1.0-alpha+20231128195327";
+const cacheName = "Edventure Studios LLC-Edventure Trek-0.1.0-alpha+20231128224542";
 const contentToCache = [
     "Build/Web.loader.js",
     "Build/Web.framework.js",
